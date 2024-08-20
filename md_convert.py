c523c5b2dc7c596bb126f8093713cf24af84c42c
Khuê Ngô <khuengo.work@gmail.com>
2024-08-20 18:11:57 +0700

    Update md_convert.py

--- md_convert.py
+++ md_convert.py
@@ -6,20 +6,6 @@ Usage:
 import argparse
 import mdtraj as md
 
-# def convert(input_file, output_file):
-#     parser = argparse.ArgumentParser(description="Convert input file extension to output file extention by using mdtraj.")
-#     parser.add_argument(input_file, '-i',
-#                         default="trajectory.h5",
-#                         help='The input file. Default is trajectory.h5.')
-#     parser.add_argument(output_file, '-o',
-#                         default='trajectory.dcd',
-#                         help='The output file. Default is trajectory.dcd.')
-#     args = parser.parse_args()
-
-#     traj = md.load(args.input_file)
-#     traj.save(args.output_file)
-
-
 
 parser = argparse.ArgumentParser(description="Convert input file extension to output file extention by using mdtraj.")
 parser.add_argument('--input_file', '-i',
